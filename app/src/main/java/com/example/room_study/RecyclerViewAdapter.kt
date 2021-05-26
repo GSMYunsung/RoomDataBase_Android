b1986ad29ade64f59d030d8eea5fc08a6474cf3c
yunsung <12345ggh81@gmail.com>
2021-05-26 21:11:38 +0900

    [UPDATE] Adapter connect

--- app/src/main/java/com/example/room_study/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/room_study/RecyclerViewAdapter.kt
@@ -1,4 +1,56 @@
 package com.example.room_study
 
-class RecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.room_study.DB.UserEntity
+import com.example.room_study.databinding.ActivityMainBinding
+import com.example.room_study.databinding.RecyclearviewRowBinding
+
+class RecyclerViewAdapter(val listener : RowClickListener) : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
+
+    var items = ArrayList<UserEntity>()
+    fun setlistData(data: ArrayList<UserEntity>){
+        this.items = data
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerViewAdapter.MyViewHolder {
+        var inflate = LayoutInflater.from(parent.context).inflate(R.layout.recyclearview_row, parent,false)
+        return MyViewHolder(inflate,listener)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewAdapter.MyViewHolder, position: Int) {
+        holder.itemView.setOnClickListener {
+            listener.onItemClikListener(items[position])
+        }
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class MyViewHolder(view : View, val listener: RowClickListener) : RecyclerView.ViewHolder(view){
+
+        val tvName = view.findViewById<TextView>(R.id.tvName)
+        val tvEmail = view.findViewById<TextView>(R.id.tvEmail)
+        val deleteUserId = view.findViewById<View>(R.id.deleteUserID)
+
+        fun bind(data : UserEntity){
+            tvName.text = data.name
+            tvEmail.text = data.email
+
+            deleteUserId.setOnClickListener {
+                listener.onItemClikListener(data)
+            }
+        }
+    }
+
+    //재활용 클릭처리
+    interface RowClickListener{
+        fun onDeleteUserClickListener(user : UserEntity)
+        fun onItemClikListener(user: UserEntity)
+    }
 }
