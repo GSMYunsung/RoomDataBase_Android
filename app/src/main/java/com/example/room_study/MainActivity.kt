9d666566d3dd4a75857d7fe02c298023edbfc7ae
yunsung <12345ggh81@gmail.com>
2021-06-15 18:27:43 +0900

    [UPDATE] Error code fixed, code Update(data input, delete, item Modify)

--- app/src/main/java/com/example/room_study/MainActivity.kt
+++ app/src/main/java/com/example/room_study/MainActivity.kt
@@ -1,25 +1,34 @@
 package com.example.room_study
 
+import android.app.Activity
 import android.graphics.drawable.ClipDrawable.VERTICAL
 import android.icu.lang.UCharacter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContentProviderCompat.requireContext
 import androidx.databinding.BaseObservable
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.Observable
 import androidx.databinding.ObservableField
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.room_study.DB.UserEntity
+import com.example.room_study.MainActivity.Companion.mainbinding
 import com.example.room_study.MainActivity.Companion.numplus
+import com.example.room_study.MainActivity.Companion.viewModel
 import com.example.room_study.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(),RecyclerViewAdapter.RowClickListener {
     companion object {
         var numplus : Int = 0
+        lateinit var mainbinding : ActivityMainBinding
+        lateinit var viewModel : MainActivityModel
     }
     lateinit var recyclerViewAdapter: RecyclerViewAdapter
-    lateinit var mainbinding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -28,6 +37,7 @@ class MainActivity : AppCompatActivity(),RecyclerViewAdapter.RowClickListener {
         mainbinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         mainbinding.btnobservable = BtnOnClick()
 
+        //객체의 초기화를 같이함. 수신객체 함수를 사용하지 않고 자신을 다시 반환함.
         mainbinding.recyclerView.apply {
             //현재 액티비티의 레이아웃을 가져온다.
             layoutManager = LinearLayoutManager(this@MainActivity)
@@ -35,28 +45,52 @@ class MainActivity : AppCompatActivity(),RecyclerViewAdapter.RowClickListener {
             recyclerViewAdapter = RecyclerViewAdapter(this@MainActivity)
             //recyclerViewAdapter를 가져온다.
             adapter = recyclerViewAdapter
-
-            var divider = DividerItemDecoration(applicationContext,VERTICAL)
-            //구분선 지정
-            addItemDecoration(divider)
-
         }
 
+        // ViewModelStoreOwner 타입이기 때문에 액티비티나 프래그먼트 this! , 생성하고자하는 뷰모델 클래스
+        viewModel = ViewModelProvider(this).get(MainActivityModel::class.java)
+        viewModel.getAllUsersObservers().observe(this, Observer {
+            recyclerViewAdapter.setlistData(ArrayList(it))
+            //이걸하면서 새로운 배열에 들어감.
+            recyclerViewAdapter.notifyDataSetChanged()
+        })
     }
 
     override fun onDeleteUserClickListener(user: UserEntity) {
-        TODO("Not yet implemented")
+        viewModel.deleteUserInfo(user)
     }
 
     override fun onItemClikListener(user: UserEntity) {
-        TODO("Not yet implemented")
+       mainbinding.fristNameInput.setText(user.name)
+       mainbinding.emailInput.setText(user.email)
+        //key , value 방식으로 가져온다.
+       mainbinding.fristNameInput.setTag(mainbinding.fristNameInput.id,user.id)
     }
 }
 
 class BtnOnClick : BaseObservable(){
-    var BtnText : ObservableField<String> = ObservableField("값 추가하기")
+    var BtnText : ObservableField<String> = ObservableField("값추가하기")
 
     fun onCilck(){
+        val name = mainbinding.fristNameInput.text.toString()
+        val email = mainbinding.emailInput.text.toString()
+
+        if(mainbinding.saveBtn.text.equals("값추가하기"))
+        {
+            val user = UserEntity(0, name, email)
+            viewModel.insertUserInfo(user)
+        }
+        else
+        {
+            val user = UserEntity(mainbinding.fristNameInput
+                .getTag(mainbinding.fristNameInput.id)
+                .toString().toInt(), name, email)
+            viewModel.updateUserInfo(user)
+        }
+
+        mainbinding.fristNameInput.setText("")
+        mainbinding.emailInput.setText("")
+
         numplus++
         BtnText.set("데이터가 " + numplus.toString() + "번 들어갔습니다.")
     }
